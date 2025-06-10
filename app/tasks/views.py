ac922d33250389d8c0d29931702806565ff495c7
SemenK <kalyakulin.semen@gmail.com>
2025-06-10 18:18:40 +0700

    .

--- app/tasks/views.py
+++ app/tasks/views.py
@@ -1,3 +1,4 @@
+import structlog
 from pydantic import ValidationError
 from rest_framework import status
 from rest_framework.request import Request
@@ -8,17 +9,21 @@ from tasks.domain.exceptions import (
     InvalidReporterID,
     InvalidAssigneeID,
     InvalidRelatedTaskIDs,
-    TitleEmptyError,
+    TitleEmptyError, DomainValidationError,
 )
 from tasks.domain.tasks.dto import TaskDTO
 from tasks.domain.use_cases.create_task import CreateTaskUsecase
+from tasks.domain.use_cases.create_user import CreateUserUsecase
+from tasks.domain.use_cases.get_user import GetUserUsecase
+from tasks.domain.users.dto import UserDTO
+from utils import log_error
+
+logger = structlog.get_logger(__name__)
 
 
-# TODO: (for the future) think about logging
-# Create your views here.
 class TaskView(APIView):
     # TODO: use dicts instead of str for response data
-    create_task_use_case: CreateTaskUsecase = None
+    create_task_usecase: CreateTaskUsecase = None
 
     def post(self, request: Request) -> Response:
         try:
@@ -29,7 +34,7 @@ class TaskView(APIView):
             return Response(f'unknown error', status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
         try:
-            self.create_task_use_case.execute(task_dto)
+            self.create_task_usecase.execute(task_dto)
         except InvalidReporterID as e:
             return Response(f'invalid reporter ID: {e.reporter_id}', status=status.HTTP_400_BAD_REQUEST)
         except InvalidAssigneeID as e:
@@ -56,3 +61,40 @@ class TaskView(APIView):
             related_task_ids=request.data.getlist('related_task_ids'),
             assignee_id=request.data['assignee_id'],
         )
+
+
+class UserView(APIView):
+    create_user_usecase: CreateUserUsecase = None
+    get_user_usecase: GetUserUsecase = None
+
+    # TODO: I could merge the try-except blocks to make the code more concise (otherwise, I need to to add except Exception for the first block)
+    def post(self, request: Request) -> Response:
+        try:
+            user_dto = self._extract_data(request)
+        except ValidationError as e:
+            return Response({'validation error': e.errors()}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            user_id = self.create_user_usecase.execute(user_dto)
+        except DomainValidationError as e:
+            return Response({'validation error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response(f'unknown error', status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        else:
+            return Response({'id': user_id}, status=status.HTTP_201_CREATED)
+
+    @log_error
+    def _extract_data(self, request: Request) -> UserDTO:
+        return UserDTO(
+            name=request.data['name'],
+        )
+
+    def get(self, request: Request, user_id: int) -> Response:
+        try:
+            user_dto = self.get_user_usecase.execute(user_id)
+        except (DomainValidationError, ValidationError) as e:
+            return Response({'error': str(e)}, status.HTTP_400_BAD_REQUEST)
+        except Exception:
+            return Response({'error': 'unknown error'}, status.HTTP_500_INTERNAL_SERVER_ERROR)
+        else:
+            return Response(data={'id': user_dto.user_id, 'name': user_dto.name})
--- app/tasks/views_tests.py
+++ app/tasks/views_tests.py
@@ -5,61 +5,91 @@ import pytest
 from pydantic import ValidationError
 from rest_framework.test import APIRequestFactory
 
-from tasks.domain.exceptions import InvalidReporterID, InvalidAssigneeID, InvalidRelatedTaskIDs, TitleEmptyError
-from tasks.views import TaskView
-
-
-@pytest.mark.parametrize(
-    "exception, status_code",
-    [
-        (InvalidReporterID(uuid.uuid4()), 400),
-        (InvalidAssigneeID(uuid.uuid4()), 400),
-        (InvalidRelatedTaskIDs([uuid.uuid4()]), 400),
-        (TitleEmptyError(uuid.uuid4()), 400),
-        (Exception, 500),
-    ],
+from tasks.domain.exceptions import (
+    InvalidReporterID,
+    InvalidAssigneeID,
+    InvalidRelatedTaskIDs,
+    TitleEmptyError,
+    DomainValidationError,
 )
-def test_task_view_returns_error_status_codes_on_usecase_exceptions(
-        exception: Exception,
-        status_code: int,
-) -> None:
-    # Arrange
-    view = TaskView.as_view(create_task_use_case=(use_case := Mock()))
-    use_case.execute.side_effect = exception
-
-    task_data = {
-        'title': 'test title',
-        'reporter_id': uuid.uuid4(),
-        'description': 'test description',
-        'related_task_ids': [uuid.uuid4(), uuid.uuid4(), uuid.uuid4()],
-        'assignee_id': uuid.uuid4(),
-        # 'task_id': None
-    }
-    request = APIRequestFactory().post("/tasks/", task_data)
-
-    # Act
-    response = view(request)
-
-    # Assert
-    assert response.status_code == status_code
+from tasks.domain.use_cases.create_user import CreateUserUsecase
+from tasks.domain.use_cases.get_user import GetUserUsecase
+from tasks.views import TaskView, UserView
 
 
-def test_task_view_returns_400_on_invalid_input_data_types() -> None:  # Arrange
-    view = TaskView.as_view(create_task_use_case=(use_case := Mock()))
-    use_case.execute.side_effect = ValidationError
+class TestCreatingTask:
 
-    task_data = {
-        'title': 'test title',
-        'reporter_id': 'invalid reporter id',
-        'description': 'test description',
-        'related_task_ids': 'invalid related task ids',
-        'assignee_id': 'invalid assignee id',
-        # 'task_id': None
-    }
-    request = APIRequestFactory().post("/tasks/", task_data)
+    @pytest.mark.parametrize(
+        "exception, status_code",
+        [
+            (InvalidReporterID(123), 400),
+            (InvalidAssigneeID(123), 400),
+            (InvalidRelatedTaskIDs([]), 400),
+            (TitleEmptyError(123), 400),
+            (Exception, 500),
+        ],
+    )
+    def test__usecase_exceptions_cause_error_status_codes(
+            self,
+            exception: Exception,
+            status_code: int,
+    ) -> None:
+        view = TaskView.as_view(create_task_usecase=(use_case := Mock()))
+        use_case.execute.side_effect = exception
+        task_data = {
+            'title': 'test title',
+            'reporter_id': uuid.uuid4(),
+            'description': 'test description',
+            'related_task_ids': [uuid.uuid4(), uuid.uuid4(), uuid.uuid4()],
+            'assignee_id': uuid.uuid4(),
+        }
+        request = APIRequestFactory().post("/tasks/", task_data)  # TODO: path seems to be missing prefix
 
-    # Act
-    response = view(request)
+        response = view(request)
 
-    # Assert
-    assert response.status_code == 400
+        assert response.status_code == status_code
+
+    def test__invalid_input_data_types_cause_400(self) -> None:
+        view = TaskView.as_view(create_task_usecase=(stub_use_case := Mock()))
+        stub_use_case.execute.side_effect = ValidationError  # TODO: use case DOESN'T CAUSE ValidationError!! Fix this
+        task_data = {
+            'title': 'test title',
+            'reporter_id': 'invalid reporter id',
+            'description': 'test description',
+            'related_task_ids': 'invalid related task ids',
+            'assignee_id': 'invalid assignee id',
+        }
+        request = APIRequestFactory().post("/tasks/", task_data)  # TODO: path seems to be missing prefix
+
+        response = view(request)
+
+        assert response.status_code == 400
+
+
+class TestGettingUser:
+
+    # TODO: add assertion for response text
+    @pytest.mark.parametrize(
+        'exception, status_code', [
+            (Exception(), 500),
+            (ValidationError('', []), 400),
+            (DomainValidationError(), 400),
+        ]
+    )
+    def test__usecase_exceptions_cause_error_status_codes(self, exception: Exception, status_code: int) -> None:
+        view = UserView.as_view(get_user_usecase=(stub_use_case := Mock(spec=GetUserUsecase)))
+        stub_use_case.execute.side_effect = exception
+        request = APIRequestFactory().get('/tasks/users/{user_id}')
+        test_user_id = 123
+
+        response = view(request, user_id=test_user_id)
+
+        assert response.status_code == status_code
+
+    def test__user_view_returns_400_on_invalid_input_data(self) -> None:
+        view = UserView.as_view(create_user_usecase=CreateUserUsecase(user_repo=Mock()))
+        request = APIRequestFactory().post('/users/', data={'name': ''}, content_type='application/json')
+
+        response = view(request)
+
+        assert response.status_code == 400
