9a1656fa5355a172c68dcfc1a8530f4e49b129fb
SemenK <kalyakulin.semen@gmail.com>
2025-06-10 17:57:03 +0700

    .

--- app/tasks/domain/exceptions.py
+++ app/tasks/domain/exceptions.py
@@ -1,3 +1,6 @@
+from typing import Sequence
+
+
 class ApplicationError(Exception):
     ...
 
@@ -7,16 +10,23 @@ class TitleEmptyError(ApplicationError):
 
 
 class InvalidReporterID(ApplicationError):
-    ...
+    def __init__(self, reporter_id: int, message: str = '') -> None:
+        super().__init__(message)
+        self.reporter_id = reporter_id
 
 
 class InvalidAssigneeID(ApplicationError):
-    ...
-
-
-class InvalidCommentIDs(ApplicationError):
-    ...
+    def __init__(self, assignee_id: int, message: str = '') -> None:
+        super().__init__(message)
+        self.assignee_id = assignee_id
 
 
 class InvalidRelatedTaskIDs(ApplicationError):
+    def __init__(self, comment_ids: Sequence[int], message: str = '') -> None:
+        """ Some or all of the related_task_ids are invalid """
+        super().__init__(message)
+        self.invalid_bunch_of_related_task_ids = comment_ids
+
+
+class DomainValidationError(ApplicationError):
     ...
---
+++ app/tasks/domain/use_cases/get_user.py
@@ -0,0 +1,16 @@
+from tasks.domain.use_cases.base import Usecase
+from tasks.domain.users.dto import UserDTO
+from tasks.domain.users.repository import UserRepository
+
+
+class GetUserUsecase(Usecase):
+    def __init__(self, user_repo: UserRepository) -> None:
+        self._user_repo = user_repo
+
+    def execute(self, user_id: int) -> UserDTO:
+        user = self._user_repo.get(user_id)
+        return UserDTO.from_user(user)
+
+
+def get_user_usecase_factory() -> GetUserUsecase:
+    return GetUserUsecase(user_repo=UserRepository())
