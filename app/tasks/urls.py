26b880914811db5ed9b5f99d3de01d5d1d177c7d
SemenK <kalyakulin.semen@gmail.com>
2025-06-13 19:40:23 +0700

    .

--- app/tasks/urls.py
+++ app/tasks/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 
 from . import views
-from .domain.use_cases.create_task import create_task_usecase_factory
 from .domain.use_cases.create_user import create_user_usecase_factory
 from .domain.use_cases.get_user import get_user_usecase_factory
 
