3dab9b100c0548e0c1d7e19cf9dd08b0855782e8
SemenK <kalyakulin.semen@gmail.com>
2025-06-13 19:53:31 +0700

    .

--- app/tasks/domain/use_cases/base.py
+++ app/tasks/domain/use_cases/base.py
@@ -6,14 +6,22 @@ import icontract
 from utils import log_error
 
 _METHOD_NAME_FOR_EXECUTION = (
-    # NOTE (SemenK): added this constant to make sure that
-    # name of this constant should stay "_METHOD_NAME_FOR_EXECUTION"
+    # NOTE (SemenK): name of this constant should stay "_METHOD_NAME_FOR_EXECUTION"
     'execute'
 )
 
 
 class Usecase(ABC):
     @icontract.require(
+        # NOTE: we need to make sure that the execution method's name
+        # changes synchronously with '_METHOD_NAME_FOR_EXECUTION' value.
+        # '_METHOD_NAME_FOR_EXECUTION' is used to dynamically wrap execution method with logging decorator
+        # Alternatives would be:
+        #   1. to explicitly wrap the method for each subclass. In this case we would have to remember to do it with each usecase
+        #   2. to make execution method here non-abstract, wrap it with the decorator and make it call .execute()
+        #       method from subclasses. In this case we lose the method's arguments' and their types' hints,
+        #       we would only see *args, **kwargs as hints when calling the execution method and would have to go
+        #       to usecase's definition to see what parameters its execution method takes
         lambda cls: hasattr(cls, _METHOD_NAME_FOR_EXECUTION),
         description=(f"To change '.{_METHOD_NAME_FOR_EXECUTION}' method's name, "
                      f"first change _METHOD_NAME_FOR_EXECUTION value.")
