c814272eed20600d78d0bdfa6b0e9ffdbb12840a
SemenK <kalyakulin.semen@gmail.com>
2025-06-15 09:22:15 +0700

    .

--- app/tasks/domain/exceptions.py
+++ app/tasks/domain/exceptions.py
@@ -10,22 +10,22 @@ class TitleEmptyError(ApplicationError):
 
 
 class InvalidReporterID(ApplicationError):
-    def __init__(self, reporter_id: int, message: str = '') -> None:
-        super().__init__(message)
+    def __init__(self, reporter_id: int) -> None:
+        super().__init__(f'invalid reporter ID: {reporter_id}')
         self.reporter_id = reporter_id
 
 
 class InvalidAssigneeID(ApplicationError):
-    def __init__(self, assignee_id: int, message: str = '') -> None:
-        super().__init__(message)
+    def __init__(self, assignee_id: int) -> None:
+        super().__init__(f'invalid assignee ID: {assignee_id}')
         self.assignee_id = assignee_id
 
 
 class InvalidRelatedTaskIDs(ApplicationError):
-    def __init__(self, comment_ids: Sequence[int], message: str = '') -> None:
+    def __init__(self, task_ids: Sequence[int]) -> None:
         """ Some or all of the related_task_ids are invalid """
-        super().__init__(message)
-        self.invalid_bunch_of_related_task_ids = comment_ids
+        super().__init__(f'some (or all) of the following related task IDs are invalid: {task_ids}')
+        self.invalid_bunch_of_related_task_ids = task_ids
 
 
 class DomainValidationError(ApplicationError):
