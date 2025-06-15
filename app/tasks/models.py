431bf341a44dd803efdb2acde5d805bde6031196
SemenK <kalyakulin.semen@gmail.com>
2025-06-15 08:50:12 +0700

    .

--- app/tasks/models.py
+++ app/tasks/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from tms_types import UserModel
+
+
+class TaskModel(models.Model):
+    title = models.CharField(max_length=200, blank=True, null=False)
+    description = models.TextField(blank=True, null=False)
+    related_tasks = models.ManyToManyField(to='TaskModel')
+    reporter = models.ForeignKey(to=UserModel, on_delete=models.PROTECT, related_name='reported_tasks')
+    assignee = models.ForeignKey(to=UserModel, null=True, on_delete=models.SET_NULL)
