856dd594c89f060d4d73e6786f7b8b0c0834ee53
SemenK <kalyakulin.semen@gmail.com>
2025-06-15 08:36:05 +0700

    .

--- app/tasks/api_tests.py
+++ app/tasks/api_tests.py
@@ -26,17 +26,50 @@ class TestUserAPI:
         assert created_user_response.json()['id'] == created_user_id
 
 
-@pytest.mark.skip
 @pytest.mark.django_db
 class TestTaskAPI:
 
     def test__when_task_is_created_api_returns_task_id(self) -> None:
-        task_data = {''}
+        test_title = 'test title'
+        test_reporter_id = self._create_user(username='foo')
+        test_description = 'test description'
+        test_related_task_ids = self._create_2_tasks(reporter_id=self._create_user('baz'))
+        test_assignee_id = self._create_user(username='bar')
 
-        response = APIClient().post('tasks/tasks/', data=task_data)
+        task_data = {
+            'title': test_title,
+            'reporter_id': test_reporter_id,
+            'description': test_description,
+            'related_task_ids': test_related_task_ids,
+            'assignee_id': test_assignee_id,
+        }
+
+        response = APIClient().post('/tasks/tasks/', data=task_data)
 
         assert response.status_code == 201
         assert str.isdigit(str(response.json()['id']))
+
+    def _create_user(self, username: str) -> int:
+        response = APIClient().post('/tasks/users/', {'name': username})
+        return response.json()['id']
+
+    def _create_2_tasks(self, reporter_id: int) -> list[int]:
+        task_data_1 = {
+            'title': 'title_1',
+            'reporter_id': reporter_id,
+        }
+        task_data_2 = {
+            'title': 'title_2',
+            'reporter_id': reporter_id,
+        }
+
+        post = APIClient().post('/tasks/tasks/', data=task_data_1)
+        created_task_ids = [
+            post.json()['id'],
+            APIClient().post('/tasks/tasks/', data=task_data_2).json()['id'],
+        ]
+
+        return created_task_ids
 
     def test__when_task_is_created_then_it_can_be_retrieved(self) -> None:
         test_title = 'test title'
@@ -53,8 +86,8 @@ class TestTaskAPI:
             'assignee_id': test_assignee_id,
         }
 
-        created_task_id = APIClient().post('tasks/tasks/', data=task_data).json()['id']
-        get_task_response = APIClient().get(f'tasks/tasks/{created_task_id}')
+        created_task_id = APIClient().post('/tasks/tasks/', data=task_data).json()['id']
+        get_task_response = APIClient().get(f'/tasks/tasks/{created_task_id}')
 
         assert get_task_response.status_code == 200
         assert get_task_response.json()['title'] == task_data['title']
@@ -62,25 +95,4 @@ class TestTaskAPI:
         assert get_task_response.json()['description'] == task_data['description']
         assert get_task_response.json()['related_task_ids'] == task_data['related_task_ids']
         assert get_task_response.json()['assignee_id'] == task_data['assignee_id']
-        assert get_task_response.json()['task_id'] == created_task_id
-
-    def _create_user(self, username: str) -> int:
-        response = APIClient().post('tasks/tasks/users/', data={'name': username})
-        return response.json()['id']
-
-    def _create_2_tasks(self, reporter_id: int) -> list[int]:
-        task_data_1 = {
-            'title': 'title_1',
-            'reporter_id': reporter_id,
-        }
-        task_data_2 = {
-            'title': 'title_2',
-            'reporter_id': reporter_id,
-        }
-
-        created_task_ids = [
-            APIClient().post('tasks/tasks/', data=task_data_1).json()['id'],
-            APIClient().post('tasks/tasks/', data=task_data_2).json()['id'],
-        ]
-
-        return created_task_ids
+        assert get_task_response.json()['id'] == created_task_id
