fc938720b99fc647a31c147fe5c408a31fd597fc
SemenK <kalyakulin.semen@gmail.com>
2025-06-22 18:04:49 +0700

    .

--- app/tasks/domain/users/user.py
+++ app/tasks/domain/users/user.py
@@ -7,19 +7,19 @@ from tasks.domain.exceptions import DomainValidationError
 class UserEntity(AggregateRoot):
     def __init__(
             self,
-            name: str,
+            name: str,  # TODO: use Name value instead (this lets remove validation logic from this class)
             user_id: int | None = None,
     ) -> None:
-        self.name = name
+        self._set_name(name)
         self._user_id = user_id
 
     @property
     def name(self) -> str:
         return self._name
 
-    @name.setter
+    @icontract.require(lambda self, value: isinstance(value, str), 'name must be str', error=DomainValidationError)
     @icontract.require(lambda self, value: len(value) > 0, 'name must be non-empty', error=DomainValidationError)
-    def name(self, value: str) -> None:
+    def _set_name(self, value: str) -> None:
         self._name = value
 
     @property
